7ede399174f81ee90f12a9ebd5d8f18d2bb9c14a
Ariasa_Prop <ikomangwidiadaariasa12@gmail.com>
2023-01-13 08:27:41 +0800

    f2

---
+++ android/src/main/java/com/ariasaproject/miningcrypto/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ariasaproject.miningcrypto;
+
+import android.app.Activity;
+import android.app.Bundle;
+
+public class MainActivity extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		setContentView(R.layout.main);
+		super.onCreate(savedInstanceState);
+	}
+}
