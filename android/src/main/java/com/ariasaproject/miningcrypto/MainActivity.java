3bab0be660b31e1c18c613f1fb17e5823f5b9e2e
Ariasa_Prop <ikomangwidiadaariasa12@gmail.com>
2023-01-14 17:11:05 +0800

    f29

--- android/src/main/java/com/ariasaproject/miningcrypto/MainActivity.java
+++ android/src/main/java/com/ariasaproject/miningcrypto/MainActivity.java
@@ -3,6 +3,7 @@ package com.ariasaproject.miningcrypto;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
