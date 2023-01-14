1b42cd6ed7a0d27a868c995b69f79ca2b392a8fe
Ariasa_Prop <ikomangwidiadaariasa12@gmail.com>
2023-01-14 15:13:43 +0800

    f23

--- android/src/main/java/com/ariasaproject/miningcrypto/MainActivity.java
+++ android/src/main/java/com/ariasaproject/miningcrypto/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends Activity {
 		final static String PREF_USERNAME = "Username";
 		final static String PREF_PASSWORD = "Password";
 		
-		EditText host_value, port_value, username_value, password_value;
+		EditText uri_value, username_value, password_value;
 		Button mining_switch;
 		
 		@Override
@@ -26,11 +26,9 @@ public class MainActivity extends Activity {
 				setContentView(R.layout.main);
 				super.onCreate(b);
 				mining_switch = (Button) findViewById(R.id.mining_switch);
-				host_value = (EditText) findViewById(R.id.host_value);
-				port_value = (EditText) findViewById(R.id.port_value);
+				uri_value = (EditText) findViewById(R.id.uri_value);
 				username_value = (EditText) findViewById(R.id.username_value);
 				password_value = (EditText) findViewById(R.id.password_value);
-				/*
 				if (b.containsKey(PREF_HOST)) {
 						host_value.setText(b.getString(PREF_HOST));
 						port_value.setText(String.valueOf(b.getShort(PREF_PORT)));
@@ -39,22 +37,46 @@ public class MainActivity extends Activity {
 						mining_switch.setEnabled(true);
 						checkAllSettings();
 				}
-				*/
 		}
 		URI curURI;
 		public void startMining(View v) {
-				String host = host_value.getText().toString();
-				int port = 8080;//Integer.parseInt(host_value.getText().toString());
+				String address = uri_value.getText().toString();
 				String username = username_value.getText().toString();
 				String password = password_value.getText().toString();
 				try {
-						curURI = new URI(String.format("%s:%d/",host,port));
+						curURI = new URI(address);
 						Toast.makeText(v.getContext(), String.format("%s %s:%s", curURI.toString(), username, password),Toast.LENGTH_SHORT).show();
 				} catch (URISyntaxException e) {
 						Toast.makeText(v.getContext(),e.getMessage(),Toast.LENGTH_SHORT).show();
 				} catch (Exception e) {
 						Toast.makeText(v.getContext(),e.getMessage(),Toast.LENGTH_SHORT).show();
 				}
+				/*
+				String url = DEFAULT_URL;
+				String auth = DEFAULT_AUTH;
+				int nThread = Runtime.getRuntime().availableProcessors();
+				double throttle = 1.0;
+				long scanTime = DEFAULT_SCAN_TIME;
+				long retryPause = DEFAULT_RETRY_PAUSE;
+				
+				if (args.length > 0 && args[0].equals("--help")) {
+					System.out.println("Usage:  java Miner [URL] [USERNAME:PASSWORD] [THREADS] [THROTTLE] [SCANTIME] [RETRYPAUSE]");
+					return;
+				}
+				
+				if (args.length > 0) url = args[0];
+				if (args.length > 1) auth = args[1];
+				if (args.length > 2) nThread = Integer.parseInt(args[2]);
+				if (args.length > 3) throttle = Double.parseDouble(args[3]);
+				if (args.length > 4) scanTime = Integer.parseInt(args[4]) * 1000L;
+				if (args.length > 5) retryPause = Integer.parseInt(args[5]) * 1000L;
+				
+				try {
+					new Miner(url, auth, scanTime, retryPause, nThread, throttle);
+				} catch (Exception e) {
+					System.err.println(e.getMessage());
+				}
+				*/
 		}
 		
 }
